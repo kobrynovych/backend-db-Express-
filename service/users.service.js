f62f7fa91922a799113db79dcfdc5afd25354d68
kobrynovych <kobrynovych@gmail.com>
2025-03-06 00:11:29 +0200

    fix auth

--- service/users.service.js
+++ service/users.service.js
@@ -1,12 +1,12 @@
 const USERS = []
 
 export const getUserByLogin = async (username) => {
-    const user = USERS.find(user => user.login === username)
+    const user = USERS.find(user => user?.login === username)
 
-    return user
+    return user || null
 }
 
-export const create = async (login, role, passwordHash) => {
+export const create = async ({login, role, passwordHash}) => {
     let newUser = { 
         id: USERS.length + 1, 
         login: login, 
